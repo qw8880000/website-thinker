49c1316d73a179f8298d563a6aa8a5cc38e22da3
qw8880000 <qw8880000@126.com>
2018-05-23 14:39:30 +0800

    [debug] nav click

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,15 @@
-function navSwitch () {
-  var $navItems = $('.nav-item > .nav-link');
-  var $current = $(this);
+$(document).ready(function () {
 
-  $navItems.removeClass('active');
-  $current.addClass('active');
-}
+  function navSwitch (event) {
+    var $navItems = $('.nav-item > .nav-link');
+    var $current = $(event.currentTarget)
+
+    $navItems.removeClass('active');
+    $current.addClass('active');
+  }
+
+  $('.nav-item > .nav-link').click(navSwitch);
+
+});
+
+
