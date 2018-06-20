ad176f7bda472cdaa56c92ec79e9fe50238a4190
qw8880000 <qw8880000@126.com>
2018-06-20 17:44:38 +0800

    [feature] gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('imagemin', function() {
-  gulp.src('images-raw/**/*')
+  return gulp.src('./images/**/*')
   .pipe(imagemin([
     imagemin.jpegtran({progressive: true}),
     imagemin.optipng({optimizationLevel: 5})
   ], {
 	  verbose: true
   }))
-  .pipe(gulp.dest('images'))
+  .pipe(gulp.dest('./build/images'))
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./scss/app.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./css'));
 });
 
 gulp.task('default', function() {
