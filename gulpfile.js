835462358d9f21d56d7675c8bf804b92877255bb
qw8880000 <qw8880000@126.com>
2018-06-20 15:58:05 +0800

    [feature] gulp imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,13 @@
 var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+
+gulp.task('imagemin', function() {
+  gulp.src('images-raw/**/*')
+  .pipe(imagemin([], {
+	  verbose: true
+  }))
+  .pipe(gulp.dest('images'))
+});
 
 gulp.task('default', function() {
   // 将你的默认的任务代码放在这
