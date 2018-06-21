dcb90ba596658dd3022e9ae1e350de86c5871e01
qw8880000 <qw8880000@126.com>
2018-06-21 09:28:41 +0800

    [feature] gulp postcss

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 
 gulp.task('imagemin', function() {
   return gulp.src('./images/**/*')
@@ -18,7 +20,15 @@ gulp.task('sass', function () {
   return gulp.src('./scss/app.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('postcss', function () {
+  return gulp.src('./css/app.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./css'));
 });
 
