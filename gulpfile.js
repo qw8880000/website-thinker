e31d63892e040f2ce7e88c5c2dca4983c719b74e
qw8880000 <qw8880000@126.com>
2018-06-21 10:26:17 +0800

    [feature] gulp js css minify

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,11 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var concat = require('gulp-concat');
+var merge = require('merge-stream');
 
 gulp.task('imagemin', function() {
   return gulp.src('./images/**/*')
@@ -26,10 +31,25 @@ gulp.task('sass', function () {
 
 gulp.task('postcss', function () {
   return gulp.src('./css/app.css')
-    .pipe(sourcemaps.init())
     .pipe(postcss([ autoprefixer() ]))
-    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./css'));
+});
+
+gulp.task('minify-css', function () {
+  return gulp.src('./css/app.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('minify-js', function () {
+  var jsWaitMinify = gulp.src('./js/*.js')
+    .pipe(uglify());
+
+  var jsMinified = gulp.src('./js/vendor/*.js');
+
+  merge(jsWaitMinify, jsMinified)
+    .pipe(concat('app.min.js'))
+    .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('default', function() {
